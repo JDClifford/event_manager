58c88bad6a826546c3534f39cf7fe941fc6c8596
John Clifford <johnclifford00@gmail.com>
2018-04-03 18:54:04 -0400

    Added method which takes zip code inputs and (using google's API) writes legislator name's to a string

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,20 +1,34 @@
-require "csv"
+require 'csv'
+require 'google/apis/civicinfo_v2'
 
-def clearn_zipcode(zipcode) #Corrects for issues in formatting with zipcode inputs
-	if zipcode == nil
-		zipcode = "00000"
-	elsif zipcode.length > 5
-		zipcode = zopcode[0..4]
-	else
-		zipcode.rjust(5,"0")
-	end
+
+def clean_zipcode(zipcode) #Corrects for issues in formatting with zipcode inputs
+	zipcode.to_s.rjust(5,"0")[0..4]
 end
 
-puts "EventManager Initialized!"
-contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+def legislators_by_zipcode(zip) #Collects the names of representatives into a string based on zipcode
+
+	civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+	civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+		begin
+			legislators = civic_info.representative_info_by_address(
+										address: zip,
+										levels: 'country',
+										roles: ['legislatorUpperBody', 'legislatorLowerBody'])
+			legislators = legislators.officials
+			legislator_names = legislators.map(&:name)
+			legislator_names = legislator_names.join(", ")
+		rescue
+			puts "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
+		end
+end
+
+puts "EventManager initialized."
+contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
+
 contents.each do |row|
 	name = row[:first_name]
-	zipcode = row[:zipcode]
-	zipcode = clearn_zipcode(zipcode)
-	puts "#{name}: #{zipcode}"
+	zipcode = clean_zipcode(row[:zipcode])
+	legislators = legislators_by_zipcode(zipcode)
+	puts "#{name} (#{zipcode}): #{legislators}"
 end
